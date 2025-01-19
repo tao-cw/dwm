f270d18ab57c24eff9f6f94673a5160dc5db16a7
taocw <taocw@foxmail.com>
2025-01-19 10:19:35 +0800

    delete xinerama

--- dwm.c
+++ dwm.c
@@ -21,6 +21,7 @@
  * To understand everything else, start reading main().
  */
 #include <X11/Xatom.h>
+#include <X11/Xft/Xft.h>
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
 #include <X11/Xutil.h>
@@ -35,10 +36,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#ifdef XINERAMA
-#include <X11/extensions/Xinerama.h>
-#endif /* XINERAMA */
-#include <X11/Xft/Xft.h>
 
 #include "drw.h"
 #include "util.h"
